3ae63c3031142078747d9d3d0cf9eb901e098b0b
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-02-18 01:21:56 +0300

    Add files via upload

--- src/grikod/_version.py
+++ src/grikod/_version.py
@@ -1,2 +1,8 @@
+from __future__ import annotations
 import sys
-__version__ = "1.0.3"
+__version__ = "1.0.5"
+__license__ = "Açık Kaynak: Open Source, MIT, Apache License 2.0"
+__description__ = "Grikod (Gri Kod, Gray Code, Grey Code)"
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/KuantumBS/grikod"
+__dependencies__ = ["python>=3.8"]
