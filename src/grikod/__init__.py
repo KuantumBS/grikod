2069726e40df90cc1a4c9c293b31aaab5d9c6a75
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-03-23 15:55:10 +0300

    Update __init__.py

--- src/grikod/__init__.py
+++ src/grikod/__init__.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 from . import  grikod
 import warnings
+
+
 def eski_fonksiyon():
+    
+    
     warnings.warn("Grikod Python 3.8-3.13 sürümlerinde sorunsuz çalışmalıdır.", DeprecationWarning)
 eski_fonksiyon()
 __version__ = "1.0.5"
