a4c020ce113c8c600e7c9a5394d7447584d307bc
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-10-29 00:24:26 +0300

    Update grikod.py

--- src/grikod/grikod.py
+++ src/grikod/grikod.py
@@ -1,19 +1,21 @@
 from __future__ import annotations
 
+
 def ikili_2_gri_kod(i2grik: str) -> str:
-    # Dizeyi tamsayıya dönüştür
+    """Converts a binary string to its corresponding Gray Code."""
+    # Convert string to integer
     i2grikod = int(i2grik, 2)
-    
-    # Gri (Gray) kodunu uygula
+
+    # Apply Gray Code transformation
     i2grikod ^= (i2grikod >> 1)
-    
-    # Sonucu ikili sayı sistemiyle döndür
+
+    # Return the result as a binary string
     return bin(i2grikod)[2:]
 
-# Kullanıcıdan ikili sayı girişi al
+
+# Get binary input from the user
 i2grik = input("Bir ikili sistem (0, 1; Örnek: 101) sayısı giriniz: ")
 gri_kod = ikili_2_gri_kod(i2grik)
 
-# Gri kod sonucunu yazdır
-# print(f"Gri Kod sayısı: {gri_kod}")
+# Print the Gray Code result
 print("Gri Kod sayısı:", gri_kod)
