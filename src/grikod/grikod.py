6f6e2daa28d0a45a4f499af8c641dc998487cc1f
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-10-28 23:35:13 +0300

    Update grikod.py

--- src/grikod/grikod.py
+++ src/grikod/grikod.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
+
+
 def ikili_2_gri_kod(i2grik: str) -> str:
     
     # Dizeyi tamsayıya dönüştür
-    
     i2grikod = int(i2grik, 2)
     
     # Gri:Gray kodunu uygula
-    
     i2grikod ^= (i2grikod >> 1)
     
     # Sonucu ikili sayı sistemiyle döndür
-    
     return bin(i2grikod)[2:]
 
 # Örnek kullanım
@@ -20,5 +19,4 @@ i2grik = input('Bir ikili sistem (0, 1; Örnek: 101) sayısı giriniz: ')
 gri_kod = ikili_2_gri_kod(i2grik)
 
 # print(f"Gri Kod sayısı: {gri_kod}")
-
 print('Gri Kod sayısı: ', gri_kod)
