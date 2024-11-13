7b797c905361c399c08eeb1c8a97d04d08ca5eb4
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-11-13 18:05:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools_scm
 
 setup(
     name='grikod',
+    use_scm_version = True,  # Bu satır version bilgisini setuptools_scm ile alır
+    requires = ["setuptools", "wheel", "setuptools_scm"],  # gerekli modüller
     version='1.0.8',
     packages=find_packages(),
     install_requires=[
