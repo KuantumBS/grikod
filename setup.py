0267692a4864b1ad2954f473ef645b82b45c0a21
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-11-13 18:21:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ setup(
     use_scm_version = True,  # Bu satır version bilgisini setuptools_scm ile alır
     requires = ["setuptools", "wheel", "setuptools_scm"],  # gerekli modüller
     version='1.0.8',
-    packages=find_packages(),
+    packages=find_packages(where="src"),  # src dizinindeki modülleri bul
+    package_dir={"": "src"},  # src dizinine yönlendirme
+    setup_requires=["setuptools", "wheel", "setuptools_scm"],
+    use_scm_version=True,  # sürüm bilgisini setuptools_scm ile al
+    include_package_data=True,  # Gerektiğinde diğer dosyaları dahil et
     install_requires=[
         # Buraya bağımlılıkları ekleyin
     ],
