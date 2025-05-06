fd8aef601c730e1f3666a2e8a7efa75a32366f13
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-05-06 18:32:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="grikod",
     use_scm_version=True,  # Sürüm bilgisini setuptools_scm ile alır
     setup_requires=["setuptools", "wheel", "setuptools_scm"],  # Gerekli kurulum bağımlılıkları
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(where="src"),  # src dizinindeki modülleri bul
     package_dir={"": "src"},  # src dizinine yönlendirme
     include_package_data=True,  # Ek dosyaları dahil et
@@ -15,4 +15,5 @@ setup(
     description="Binary to Gray code conversion package for efficient data encoding.",
     url="https://github.com/KuantumBS/grikod",
     license="MIT",
+    python_requires='>=3.9',
 )
