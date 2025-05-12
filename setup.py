defdc56c92f0cd653ba9cd29121947dd2087d6db
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-05-12 11:25:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="grikod",
     use_scm_version=True,  # Sürüm bilgisini setuptools_scm ile alır
     setup_requires=["setuptools", "wheel", "setuptools_scm"],  # Gerekli kurulum bağımlılıkları
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(where="src"),  # src dizinindeki modülleri bul
     package_dir={"": "src"},  # src dizinine yönlendirme
     include_package_data=True,  # Ek dosyaları dahil et
